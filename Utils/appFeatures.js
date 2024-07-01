1966d30b71579e68f9a525efdf077fb0d1b4c282
unicoinexchange <unicoinexchangeltd@gmail.com>
2024-07-01 21:25:19 +0100

    appfeatures update

--- Utils/appFeatures.js
+++ Utils/appFeatures.js
@@ -6,7 +6,7 @@ exports.createOTP = async ( operator ) => {
     const OTP = crypto.randomBytes(2).toString("hex");
     operator.otpToken = crypto.createHash("sha256").update(OTP).digest("hex");
 
-    operator.otpExpires = Date.now() + 10 * 60 * 60;
+    operator.otpExpires = Date.now() + 2 * 60 * 1000;
     return OTP
 };
 
