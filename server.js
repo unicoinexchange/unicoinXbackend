8bb7472acf893bb491745ea416905577fbb36cfc
Acha-Goodness <acharious1668@gmail.com>
2025-11-15 18:09:14 +0100

    server configuration update

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ process.on("uncaughtException", err => {
 dotenv.config({ path: "./config.env"});
 const app = require("./app");
 
-const DB = process.env.DATABASE
+const DB = process.env.DATABASE.replace(
+    "<PASSWORD>",
+    process.env.DATABASE_PASSWORD
+)
 
 mongoose.connect(DB, {
     // useNewUrlParser: true,
